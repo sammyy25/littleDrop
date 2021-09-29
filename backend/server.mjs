b45df25eaa7e534972e0285bce2a0cabc09ee2db
Mudathir Tijani <66947139+sammyy25@users.noreply.github.com>
2021-09-29 22:22:18 +0100

    update

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,38 +1,3 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const path = require('path');
-// // const config = require('config');
-// // const mondoDb = require('mongodb')
-// require('dotenv').config()
-
-// const app = express();
-// app.use(express.json());
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-// app.post("/post", (req, res) => {
-//   console.log("Connected to React");
-//   res.redirect("/");
-// });
-// app.use(express.static('public'))
-// app.get('/', (req, res) => {
-//   res.sendFile(__dirname + '/client/public/index.html')
-// });
-// if(process.env.NODE_ENV === 'production') {
-//     app.use(express.static('client/build'));
-//     app.get('*', (req, res) => {
-// res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-//     });
-// }
-
-// const dbURI = config.get('dbURI');
-  // const port = process.env.PORT || 8080;
-//  mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-//   .then((result) => app.listen(port))
-//   .catch((err) => console.log(err));
-
-
-// app.listen(port, () => console.log("server is running"))
 
 /***********************new Project */
 
